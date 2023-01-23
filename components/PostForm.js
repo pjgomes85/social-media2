36ee78c1d23c614dcbb8f67f3c05c81ee10bcfca
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-23 20:09:57 +0000

    add share button

--- components/PostForm.js
+++ components/PostForm.js
@@ -3,13 +3,44 @@ import Card from "./Card";
 export default function PostForm() {
   return (
     <Card>
-      <div className="flex gap-3">
+      <div className="flex gap-2">
         <div>
           <div className="w-12 rounded-full overflow-hidden">
             <img src="https://scontent-lhr8-1.xx.fbcdn.net/v/t1.6435-9/44481318_2264696876897414_1989818533889966080_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=ec5PxzyGHIcAX8FJ4EV&tn=bMn87lzrdFr5XjvA&_nc_ht=scontent-lhr8-1.xx&oh=00_AfBuKXkH4sTovfpA8NyNLO57dRtnUzrv4xjdiMWLUe6RTw&oe=63F653E3" alt="" />
           </div>
         </div>
-        <textarea className="grow py-3" placeholder={'Whats on your mind, Paulo?'} />
+        <textarea className="grow p-3 h-14" placeholder={'Whats on your mind, Paulo?'} />
+      </div>
+      <div className="flex gap-6 items-center mt-2">
+
+          <div>
+            <button className="flex gap-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+              </svg>
+              People</button>
+          </div>
+
+          <div>
+            <button className="flex gap-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
+              </svg>
+              Check In</button>
+          </div>
+
+          <div>
+            <button className="flex gap-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
+              </svg>
+              Mood</button>
+          </div>
+          <div className="grow text-right">
+            <button className="bg-socialBlue text-white px-6 py-1 rounded-md ">
+              Share
+            </button>
+          </div>
       </div>
     </Card>
   );
