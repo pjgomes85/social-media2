4bb145f20240ed4df21f353782239b90942ac854
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-24 14:38:22 +0000

    dropdown menu working

--- components/PostCard.js
+++ components/PostCard.js
@@ -26,11 +26,20 @@ export default function PostCard() {
           <p className="text-gray-500 text-sm">1 hours ago</p>
         </div>
         <div>
-          <button className="text-gray-400" onClick={openDropdown}>
+        {!dropdownOpen && (
+            <button className="text-gray-400" onClick={openDropdown}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
             </svg>
           </button>
+          )}
+          {dropdownOpen && (
+            <button className="text-gray-400" onClick={openDropdown}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+          </button>
+          )}
           <ClickOutHandler onClickOut={handleClickOutsideDropdown}>
             <div className="relative">
               {dropdownOpen && (
