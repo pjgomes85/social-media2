8986984c9199bced4d41c077bc197e6127b2d3e2
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-24 10:04:35 +0000

    adding like, comment, share + comment text at the end of the post

--- components/PostCard.js
+++ components/PostCard.js
@@ -9,8 +9,44 @@ export default function PostCard() {
           <Avatar />
         </div>
         <div>
-          <p><a className="font-semibold">Paulo Gomes</a> shared a <a className="text-socialBlue">album</a></p>
+          <p>
+            <a className="font-semibold">Paulo Gomes</a> shared a <a className="text-socialBlue">album</a>
+          </p>
           <p className="text-gray-500 text-sm">1 hours ago</p>
+        </div>
+      </div>
+      <div>
+        <p className="my-3 text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident asperiores tenetur repellat labore quod, nesciunt veniam earum? Eveniet, dolore itaque velit non cumque optio aliquid, quam quia, voluptatibus adipisci neque.</p>
+        <div className="rounded-nd  overflow-hidden">
+          <img src="https://images.unsplash.com/photo-1562760157-c05fe30e2e8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" />
+        </div>
+      </div>
+      <div className="flex mt-4 gap-8">
+        <button className="flex gap-2 items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+          </svg>
+          72
+        </button>
+        <button className="flex gap-2 items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+          </svg>
+          13
+        </button>
+        <button className="flex gap-2 items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+          </svg>
+          3
+        </button>
+      </div>
+      <div className="flex mt-4 gap-3">
+        <div>
+          <Avatar />
+        </div>
+        <div className="border grow rounded-full">
+          <textarea className="block w-full p-3 h-12 px-4 overflow-hidden rounded-full" placeholder="Leave comment"/>
         </div>
       </div>
     </Card>
--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ import PostCard from "@/components/PostCard"
 export default function Home() {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
-      <div className='w-1/3'>
+      <div className='w-3/12'>
         <Sidebar/>
       </div>
-      <div className='grow'>
+      <div className='w-9/12'>
         <PostForm />
         <PostCard />
       </div>
