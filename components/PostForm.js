28ea8a4f6c143e553a2d709b63394dc79d6d0a29
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-24 08:15:04 +0000

    add Avatar Div File js

--- components/PostForm.js
+++ components/PostForm.js
@@ -1,3 +1,4 @@
+import Avatar from "./Avatar";
 import Card from "./Card";
 
 export default function PostForm() {
@@ -5,9 +6,7 @@ export default function PostForm() {
     <Card>
       <div className="flex gap-2">
         <div>
-          <div className="w-12 rounded-full overflow-hidden">
-            <img src="https://scontent-lhr8-1.xx.fbcdn.net/v/t1.6435-9/44481318_2264696876897414_1989818533889966080_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=ec5PxzyGHIcAX8FJ4EV&tn=bMn87lzrdFr5XjvA&_nc_ht=scontent-lhr8-1.xx&oh=00_AfBuKXkH4sTovfpA8NyNLO57dRtnUzrv4xjdiMWLUe6RTw&oe=63F653E3" alt="" />
-          </div>
+          <Avatar />
         </div>
         <textarea className="grow p-3 h-14" placeholder={'Whats on your mind, Paulo?'} />
       </div>
