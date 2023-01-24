cc63a3622e1f80fcb59f91c42da9b5e90a21c99f
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-24 15:44:17 +0000

    add hover effect with active and non active elements

--- components/PostCard.js
+++ components/PostCard.js
@@ -34,7 +34,7 @@ export default function PostCard() {
           </button>
           )}
           {dropdownOpen && (
-            <button className="text-gray-400" onClick={openDropdown}>
+            <button className="text-gray-400">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
             </svg>
