be9233c8369db9e5593b03837eebcb456869ef46
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-25 19:10:28 +0000

    adding login page

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import Sidebar from "./Sidebar";
 
-export default function Layout({children}) {
+export default function Layout({children, hideSidebar}) {
   return (
     <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
-      <div className='w-3/12'>
+      {!hideSidebar && (
+        <div className='w-3/12'>
         <Sidebar/>
       </div>
-      <div className='w-9/12'>
+      )}
+      <div className={hideSidebar ? 'w-full' : 'w-9/12'}>
         {children}
       </div>
     </div>
