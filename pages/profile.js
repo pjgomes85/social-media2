f03e7434d17f51385f3efe5c7723c3ee47202bcb
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-25 16:56:23 +0000

    add photos page

--- pages/profile.js
+++ pages/profile.js
@@ -119,9 +119,20 @@ export default function ProfilePage() {
      {isPhotos && (
       <div>
         <Card>
-          <h2>
-            My Photos
-          </h2>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+              <img src="https://images.unsplash.com/photo-1611691934391-5a8805e0bd1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" className="src" />
+            </div>
+            <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+              <img src="https://images.unsplash.com/photo-1591086857293-497543d75466?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" className="src" />
+            </div>
+            <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+              <img src="https://images.unsplash.com/photo-1660661713294-45b957739a30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80" alt="" className="src" />
+            </div>
+            <div className="rounded-md overflow-hidden h-48 flex items-center shadow-md">
+              <img src="https://images.unsplash.com/photo-1606215767711-263080a6b4b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" className="src" />
+            </div>
+          </div>
         </Card>
       </div>
      )}
