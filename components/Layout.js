2103b7938f8b2bb9fb8d6e0547b4cf148e5e9abe
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-26 14:45:06 +0000

    adjusting the navbar for mobile versio

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({children, hideSidebar}) {
   return (
     <div className='md:flex mt-4 max-w-4xl mx-auto gap-6'>
       {!hideSidebar && (
-        <div className='fixed md:static w-full md:w-3/12 -mb-5'>
+        <div className='fixed md:static bottom-0 w-full md:w-3/12 -mb-5'>
         <Sidebar/>
       </div>
       )}
