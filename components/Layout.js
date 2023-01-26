61b57890135e5f3db2969d29b7a2eb29e05533bb
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-26 16:41:48 +0000

    adjustments in photos page for mobile view

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({children, hideSidebar}) {
     rightColumnClasses += 'mx-4 md:mx-0 md:w-9/12';
   }
   return (
-    <div className='md:flex mt-4 max-w-4xl mx-auto gap-6'>
+    <div className='md:flex mt-4 max-w-4xl mx-auto gap-6 mb-24'>
       {!hideSidebar && (
         <div className='fixed md:static bottom-0 w-full md:w-3/12 -mb-5'>
         <Sidebar/>
