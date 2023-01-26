82102ea7cbca1df6ea4b3f0edc434821b01a7063
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-26 09:59:12 +0000

    login page prototype done

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,13 @@ import Sidebar from "./Sidebar";
 
 export default function Layout({children, hideSidebar}) {
   return (
-    <div className='flex mt-4 max-w-4xl mx-auto gap-6'>
+    <div className='md:flex mt-4 max-w-4xl mx-auto gap-6'>
       {!hideSidebar && (
         <div className='w-3/12'>
         <Sidebar/>
       </div>
       )}
-      <div className={hideSidebar ? 'w-full' : 'w-9/12'}>
+      <div className={hideSidebar ? 'w-full' : 'md:w-9/12'}>
         {children}
       </div>
     </div>
