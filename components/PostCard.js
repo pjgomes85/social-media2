b2f601fe4769c5dc385978afa3549b9600c7673b
Paulo Gomes <pjgomes85@hotmail.com>
2023-02-17 11:22:08 +0000

    users can add photos in the comments

--- components/PostCard.js
+++ components/PostCard.js
@@ -15,6 +15,7 @@ export default function PostCard({id,content,created_at,photos,profiles:authorPr
   const {profile:myProfile} = useContext(UserContext);
   const [likes, setLikes] = useState([]);
   const [comments, setComments] = useState([]);
+  const [upload, setUpload] = useState([]);
   const [commentText, setCommentText] = useState('');
   const supabase = useSupabaseClient();
 
@@ -84,7 +85,29 @@ export default function PostCard({id,content,created_at,photos,profiles:authorPr
     .then(result => {
       fetchComments();
       setCommentText('');
+      setUpload([]);
     })
+  }
+
+  async function addPhotos(ev) {
+    const files = ev.target.files;
+    if (files.length > 0) {
+      // setIsUploading(true);
+      for (const file of files) {
+        const newName = Date.now() + file.name;
+        const result = await supabase
+        .storage
+        .from('photos')
+        .upload(newName, file);
+        if (result.data) {
+          const url = process.env.NEXT_PUBLIC_SUPABASE_URL + '/storage/v1/object/public/photos/' + result.data.path
+          setUpload(prevUploads => [...prevUploads,url]);
+        } else {
+          console.log(result)
+        }
+       }
+      //  setIsUploading(false);
+    }
   }
 
 
@@ -191,7 +214,7 @@ export default function PostCard({id,content,created_at,photos,profiles:authorPr
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
           </svg>
-          13
+          {comments.length}
         </button>
         <button className="flex gap-2 items-center">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -211,28 +234,43 @@ export default function PostCard({id,content,created_at,photos,profiles:authorPr
               onChange={ev => setCommentText(ev.target.value)}
               className="block w-full p-3 h-12 px-4 overflow-hidden rounded-full" placeholder="Leave comment"/>
           </form>
-          <button className="absolute top-3 right-3 text-gray-500">
+          <label className="absolute top-3 right-3 text-gray-500">
+          <input type="file" className="hidden" onChange={addPhotos}/>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
               <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
             </svg>
-          </button>
+
+          </label>
+
         </div>
       </div>
       <div>
         {comments.length > 0 && comments.map(comment => (
-          <div key={true} className="mt-2 flex gap-2 items-center">
-            <Avatar url={comment.profiles.avatar} />
+          <div key={Comment} className="mt-2 flex gap-2 items-center">
+            <Avatar url={comment.profiles.avatar} href={'/profile/'+ comment.profiles.id} />
             <div className="bg-gray-300 py-2 px-4 rounded-xl">
               <div>
               <Link href={'/profile/'+ comment.profiles.id}>
-                <span className="hover:underline font-semibold">
+                <span className="hover:underline font-semibold mr-1">
                   {comment.profiles.name}
                 </span>
               </Link>
-              <ReactTimeAgo date={(new Date(comment.created_at)).getTime()} />
+              <span className="text-sm text-gray-400">
+                <ReactTimeAgo timeStyle="twitter" date={(new Date(comment.created_at)).getTime()} />
+              </span>
               </div>
               <p className="text-sm">{comment.content}</p>
+              {upload.length > 0 && (
+                <div className="flex gap-2">
+                  {upload.map(uploads => (
+                    // eslint-disable-next-line react/jsx-key
+                    <div className="mt-2">
+                      <img src={uploads} alt="" className="w-auto h-24 rounded-md" />
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         ))}
