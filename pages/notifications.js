4f6820d340897483779ea4b0c16a610e362c9301
Paulo Gomes <pjgomes85@hotmail.com>
2023-02-22 17:10:50 +0000

    adding notifications

--- pages/notifications.js
+++ pages/notifications.js
@@ -1,9 +1,39 @@
 import Avatar from "@/components/Avatar";
 import Card from "@/components/Card";
 import Layout from "@/components/Layout";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function NotificationsPage() {
+  const [likes, setLikes] = useState([]);
+  const supabase = useSupabaseClient();
+
+useEffect(() => {
+fetchLikes()
+
+}, []);
+
+
+
+
+  function fetchLikes() {
+    supabase.from('likes')
+    .select()
+    .eq('post_id')
+    .then(result =>
+      setLikes(result.data)
+    )
+  }
+
+  const isLikedByMe = !!likes?.find(like =>
+    like.user_id === myProfile?.id
+  )
+
+
+
+
+
   return (
     <Layout>
       <h1 className="text-6xl mb-4 text-gray-400">Notifications</h1>
