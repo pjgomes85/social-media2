fc818924ca9648c837519192ab59fdf9837c4704
Paulo Gomes <pjgomes85@hotmail.com>
2023-02-27 08:58:51 +0000

    working on notifications

--- pages/notifications.js
+++ pages/notifications.js
@@ -5,7 +5,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function NotificationsPage() {
+export default function NotificationsPage({id,content,created_at,photos,profiles:authorProfile}) {
   const [likes, setLikes] = useState([]);
   const supabase = useSupabaseClient();
 
@@ -17,18 +17,14 @@ fetchLikes()
 
 
 
-  function fetchLikes() {
-    supabase.from('likes')
-    .select()
-    .eq('post_id')
-    .then(result =>
-      setLikes(result.data)
-    )
-  }
-
-  const isLikedByMe = !!likes?.find(like =>
-    like.user_id === myProfile?.id
+function fetchLikes() {
+  supabase.from('likes')
+  .select()
+  .eq('post_id', id)
+  .then(result =>
+    setLikes(result.data)
   )
+}
 
 
 
@@ -44,6 +40,7 @@ fetchLikes()
               <Avatar />
             </Link>
             <div>
+              {likes?.length}
               <Link href={'/profile'} className="font-semibold mr-1 hover:underline">Paulo Gomes</Link>
                liked
               <Link href="/profile/photos" className={"ml-1 text-socialBlue"}> your foto</Link>
