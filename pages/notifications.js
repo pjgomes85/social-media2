3a60789eb00fa3445dc758bb9dfcd0d170a499d6
Paulo Gomes <pjgomes85@hotmail.com>
2023-03-06 13:06:03 +0000

    converting id into the danem supabase

--- pages/notifications.js
+++ pages/notifications.js
@@ -1,30 +1,43 @@
 import Avatar from "@/components/Avatar";
 import Card from "@/components/Card";
 import Layout from "@/components/Layout";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import PostCard from "@/components/PostCard";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function NotificationsPage({id,content,created_at,photos,profiles:authorProfile}) {
-  const [likes, setLikes] = useState([]);
+
+export default function NotificationsPage({profiles:authorProfile}) {
+  const [posts, setPosts] = useState([]);
+  const session = useSession();
   const supabase = useSupabaseClient();
 
-useEffect(() => {
-fetchLikes()
+  useEffect(() => {
+    if (!session?.user?.id) {
+      return;
+    }
+    supabase.from('likes')
+    .select('post_id')
+    .eq('user_id', session.user.id)
+    .then(result => {
+      const postIds = result.data.map(item => item.post_id)
+      supabase.from('posts').select('*, profiles(*)').in('id', postIds)
+      .then(result => setPosts(result.data))
+    })
+  }, [session?.user?.id]);
 
-}, []);
 
 
+// function fetchLikes() {
 
+//   supabase.from('likes','profiles')
+//   .select()
+//   .then(result =>
+//     setLikes(result.data)
+//   )
 
-function fetchLikes() {
-  supabase.from('likes')
-  .select()
-  .eq('post_id', id)
-  .then(result =>
-    setLikes(result.data)
-  )
-}
+// }
+
 
 
 
@@ -34,13 +47,21 @@ function fetchLikes() {
     <Layout>
       <h1 className="text-6xl mb-4 text-gray-400">Notifications</h1>
       <Card noPadding={true}>
+
+
+            {posts?.length > 0 && posts.map(post => (
+        <div key={post.id}>
+          <Link {...post} href={'/profile'} className="font-semibold mr-1 hover:underline">{post.author}</Link>
+        </div>
+      ))}
+
+
         <div className="">
           <div className="flex gap-2 items-center border-b border-b-gray-100 p-4">
             <Link href={'/profile'}>
               <Avatar />
             </Link>
             <div>
-              {likes?.length}
               <Link href={'/profile'} className="font-semibold mr-1 hover:underline">Paulo Gomes</Link>
                liked
               <Link href="/profile/photos" className={"ml-1 text-socialBlue"}> your foto</Link>
